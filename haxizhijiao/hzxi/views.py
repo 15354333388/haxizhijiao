dd3056ea7ac06b724cc46a76bbf355ded9291b15
longxiang2 <43200028+longxiang2@users.noreply.github.com>
2018-12-28 18:01:21 +0800

    实现简单的注册登陆功能

--- haxizhijiao/hzxi/views.py
+++ haxizhijiao/hzxi/views.py
@@ -1,3 +1,5 @@
+import simplejson
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
@@ -7,7 +9,7 @@ from . import models
 
 
 # index，if not login, must login in index, only and receive method of 'get'
-@api_view(['GET'])
+# @api_view(['GET'])
 # checked session, if user has login, it will redirect own page
 # @permissions.is_login
 def index(request):
@@ -15,45 +17,67 @@ def index(request):
 
 
 # finish user login, only receive method of 'post'
-@api_view(['POST'])
+# @api_view(['POST'])
 # @permissions.verify_permission # check permissions
 def login(request):
-    return HttpResponse('sss')
-# finsih user register, only receive method of 'post'
-@api_view(['POST', 'GET'])
-def register(request):
-    print(request.get_host())
-    print('qawsqaws')
     data = {
         'status': 'OK',
         'data': ''
     }
-    print('1234')
+    print('------------------', request.method)
+    if request.is_ajax():
+        body = json.loads(request.body.decode(encoding='utf-8'))
+        u_name = body.get('u_name')
+        u_pwd = body.get('u_pwd')
+        print('---', u_pwd, u_name)
+        query = models.User.objects.filter(u_name=u_name, u_pwd=u_pwd)
+        if query:
+            data['data'] = 'login successed'
+        else:
+            data['status'] = 'error'
+            data['data'] = 'login failed'
+    return JsonResponse(data)
+
+
+# finsih user register, only receive method of 'post'
+# @api_view(['POST', 'GET'])
+def register(request):
+    data = {
+        'status': 'OK',
+        'data': ''
+    }
     if request.method == 'GET':
         u_pid = str(request.GET.get('u_pid', ''))
-        print(u_pid)
         if u_pid:
-           u_name = models.User.objects.get(u_pid=u_pid).u_name
+           query = models.User.objects.get(u_pid=u_pid)
+           u_name = query.u_name
            data['u_name'] = u_name
+           if query.u_pwd:
+               data['status'] = 'error'
+               data['data'] = '工号重复'
         else:
             data['status'] = 'error'
         return JsonResponse(data)
     if request.method == 'POST':
-        print('54321')
-        u_pid = request.POST.get('u_pid', '')
-        u_name = request.POST.get('u_name', '')
-        query = models.User.objects.get(u_name=u_name, u_pid=u_pid)
-        print(request.POST.get('u_pwd', ''))
-        query.u_pwd = request.POST.get('u_pwd', '')
-        query.save()
-    return JsonResponse(data)
-    # return redirect('')
-    #
-    # return HttpResponse('Register')
+        body = json.loads(request.body.decode(encoding='utf-8'))
+        # a = 'ok' if request.is_ajax() else 'no'
+        u_pid = body.get('u_pid')
+        u_name = body.get('u_name')
+        u_pwd = body.get('u_pwd')
+        try:
+            query = models.User.objects.get(u_name=u_name, u_pid=u_pid)
+            print(request.POST.get('u_pwd', ''))
+            query.u_pwd = u_pwd
+            query.save()
+            data['data'] = 'register successed'
+        except Exception as err:
+            data['status'] = 'error'
+            data['data'] = 'register failed'
+        return JsonResponse(data)
 
 
 # finish user update informations, only receive method of 'update'
-@api_view(['UPDATE'])
+# @api_view(['UPDATE'])
 def update(request):
     pass
 
