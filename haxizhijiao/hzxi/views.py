81d96165a5a4ad17705c2f231f720410076d60da
longxiang2 <43200028+longxiang2@users.noreply.github.com>
2018-12-29 13:08:32 +0800

    实现django ajax post 和 csrf 功能

--- haxizhijiao/hzxi/views.py
+++ haxizhijiao/hzxi/views.py
@@ -2,6 +2,7 @@ import simplejson
 import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from . import permissions
 from . import models
@@ -17,63 +18,86 @@ def index(request):
 
 
 # finish user login, only receive method of 'post'
-# @api_view(['POST'])
+@api_view(['POST'])
 # @permissions.verify_permission # check permissions
 def login(request):
     data = {
         'status': 'OK',
+        'msg': '',
         'data': ''
     }
-    print('------------------', request.method)
     if request.is_ajax():
-        body = json.loads(request.body.decode(encoding='utf-8'))
+        # body = json.loads(request.body.decode(encoding='utf-8'))
+        for i in request.data.keys():
+            body = json.loads(i)
         u_name = body.get('u_name')
         u_pwd = body.get('u_pwd')
-        print('---', u_pwd, u_name)
-        query = models.User.objects.filter(u_name=u_name, u_pwd=u_pwd)
-        if query:
-            data['data'] = 'login successed'
-        else:
+        try:
+            query = models.User.objects.get(u_name=u_name, u_pwd=u_pwd)
+            data['msg'] = 'login successed'
+            return JsonResponse(data, status=status.HTTP_200_OK)
+        except models.User.DoesNotExist or models.User.MultipleObjectsReturned:
             data['status'] = 'error'
             data['data'] = 'login failed'
-    return JsonResponse(data)
+            return JsonResponse(data, status=status.HTTP_404_NOT_FOUND)
 
 
 # finsih user register, only receive method of 'post'
-# @api_view(['POST', 'GET'])
+@api_view(['POST', 'GET'])
 def register(request):
     data = {
         'status': 'OK',
+        'msg': '',
         'data': ''
     }
     if request.method == 'GET':
         u_pid = str(request.GET.get('u_pid', ''))
         if u_pid:
-           query = models.User.objects.get(u_pid=u_pid)
-           u_name = query.u_name
-           data['u_name'] = u_name
-           if query.u_pwd:
-               data['status'] = 'error'
-               data['data'] = '工号重复'
+            try:
+                query = models.User.objects.get(u_pid=u_pid)
+                u_name = query.u_name
+                data['u_name'] = u_name
+                if query.u_pwd:
+                    data['status'] = 'error'
+                    data['msg'] = '工号重复'
+                if not data['msg']:
+                    return JsonResponse(data, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse(data, status=status.HTTP_200_OK)
+            except models.User.MultipleObjectsReturned or models.User.DoesNotExist:
+                data['status'] = 'error'
+                data['msg'] = 'register failed'
+                return JsonResponse(data, status=status.HTTP_400_BAD_REQUEST)
         else:
             data['status'] = 'error'
-        return JsonResponse(data)
+            data['msg'] = '没有这个工号'
+            return JsonResponse(data, status=status.HTTP_404_NOT_FOUND)
     if request.method == 'POST':
-        body = json.loads(request.body.decode(encoding='utf-8'))
+        # print(request.data)
+        # body = json.loads(request.body.decode(encoding='uf-8'))
+        dict = request.data
+        for i in dict.keys():
+            body = json.loads(i)
+            print(body, type(body))
+            break
         # a = 'ok' if request.is_ajax() else 'no'
         u_pid = body.get('u_pid')
         u_name = body.get('u_name')
         u_pwd = body.get('u_pwd')
-        try:
-            query = models.User.objects.get(u_name=u_name, u_pid=u_pid)
-            print(request.POST.get('u_pwd', ''))
-            query.u_pwd = u_pwd
-            query.save()
-            data['data'] = 'register successed'
-        except Exception as err:
+        if u_pid and u_name and u_pwd:
+            try:
+                query = models.User.objects.get(u_name=u_name, u_pid=u_pid)
+                query.u_pwd = u_pwd
+                query.save()
+                data['data'] = 'register successed'
+            except models.User.DoesNotExist or models.User.MultipleObjectsReturned:
+                data['status'] = 'error'
+                data['data'] = 'register failed'
+                return JsonResponse(data, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(data, status=status.HTTP_201_CREATED)
+        else:
             data['status'] = 'error'
-            data['data'] = 'register failed'
-        return JsonResponse(data)
+            data['msg'] = '账号，密码，工会都不能为空'
+            return JsonResponse(data, status=status.HTTP_400_BAD_REQUEST)
 
 
 # finish user update informations, only receive method of 'update'
